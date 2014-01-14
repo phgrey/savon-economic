ab7f603c31811ac6865dbeda1cb1f90c0a071a65
phgrey <phgrey@gmail.com>
2014-01-14 17:46:51 +0200

    products by group

--- lib/savon/economic/model/product_group.rb
+++ lib/savon/economic/model/product_group.rb
@@ -1,5 +1,12 @@
 module Savon::Economic::Model
   class ProductGroup < Base
 
+
+    class_operations :get_products
+    def self.get_products group_id
+      ids = super(self_handle => {id_number => group_id})
+      ids && ids[:product_handle] || []
+    end
+
   end
 end
